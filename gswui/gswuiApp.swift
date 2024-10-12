d3df723865dd0c09297ef0d290a50e3db642bcdb
john davis <davis@doom.lan>
2024-10-12 18:24:27 -0400

    Initial Commit

---
+++ gswui/gswuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gswuiApp.swift
+//  gswui
+//
+//  Created by john davis on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct gswuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
