f1bfad70b76ded427ff9eb218fea42d70e8dccf6
john davis <davis@doom.lan>
2024-10-12 18:33:53 -0400

    a spritekit scene loaded from contentview

--- gswui/ContentView.swift
+++ gswui/ContentView.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import _SpriteKit_SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        
+        // define a scene
+        var scene: SKScene {
+            let scene = GameScene()
+            scene.size = CGSize(width: screenWidth, height: screenHeight)
+            
+            scene.backgroundColor = .clear
+            scene.scaleMode = .aspectFill
+            
+            return scene
+        }
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            SpriteView(scene: scene) 
         }
         .padding()
     }
---
+++ gswui/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  gswui
+//
+//  Created by john davis on 10/12/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        print("game scene loaded")
+    }
+}
